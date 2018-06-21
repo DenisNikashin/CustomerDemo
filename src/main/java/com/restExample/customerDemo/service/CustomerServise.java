74aad45fbe8e3248f82508596d5d326c948a8568
DenisNikashin <denis.n.82.dn@gmail.com>
2018-06-22 00:09:52 +0300

    created service layer

---
+++ src/main/java/com/restExample/customerDemo/service/CustomerServise.java
@@ -0,0 +1,12 @@
+package com.restExample.customerDemo.service;
+
+import com.restExample.customerDemo.model.Customer;
+
+import java.util.List;
+
+public interface CustomerServise {
+    Customer getById(Long id);
+    void save(Customer customer);
+    void delete(Long id);
+    List<Customer> getAll();
+}
---
+++ src/main/java/com/restExample/customerDemo/service/CustomerServiseImpl.java
@@ -0,0 +1,35 @@
+package com.restExample.customerDemo.service;
+
+import com.restExample.customerDemo.model.Customer;
+import com.restExample.customerDemo.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerServiseImpl implements CustomerServise {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Override
+    public Customer getById(Long id) {
+        return customerRepository.getOne(id);
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    @Override
+    public void delete(Long id) {
+        customerRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Customer> getAll() {
+        return customerRepository.findAll();
+    }
+}
